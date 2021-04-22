55dfb9539eea938fa4fbe5cbe458a9b4c60ae1db
jacobkosmart <jacobkosmart@gmail.com>
2021-04-22 20:38:30 +0900

    add js_01_up to math

---
+++ _posts/blank.js
@@ -0,0 +1,6 @@
+// string.prototype.indexOf()
+const result = 'Hello world'.indexOf('world')
+console.log(result) //
+// 문자 데이터가 없으면 -1이 나옴
+const noResult = 'Hello world'.indexOf('programming')
+console.log(noResult)
