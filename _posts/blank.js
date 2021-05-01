dddbae99936d2952c076442aa9df19d397cef189
jacobkosmart <jacobkosmart@gmail.com>
2021-05-01 20:51:52 +0900

    upload JS_04

--- _posts/blank.js
+++ _posts/blank.js
@@ -1,10 +1,28 @@
-function sum(x,y ) {
-  if (x < 2) {
-    return
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i >= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false
   }
-  return x + y
+  return true;
 }
 
-console.log(sum(7, 3)) // 10
+
+function solution(arr) {
+  let answer = [];
+  for (let x of arr) {
+    let res = 0
+    while (x) {
+      let t = x % 10
+      res = res * 10 + t 
+      x = parseInt(x / 10)
+    }
+    if (isPrime(res)) answer.push(res)
+  }
+  return answer;
+}
 
 
+
+
+let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+console.log(solution(arr));
