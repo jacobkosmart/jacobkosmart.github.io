a03993343722e73e21a6be6889d89d0791b8522e
jacobkosmart <jacobkosmart@gmail.com>
2021-07-21 10:39:28 +0900

    add JS array

--- _posts/blank.js
+++ _posts/blank.js
@@ -1,28 +1,22 @@
-function isPrime(num) {
-  if (num === 1) return false;
-  for (let i = 2; i >= parseInt(Math.sqrt(num)); i++) {
-    if (num % i === 0) return false
+// forEach, map, filter, reduce
+
+
+// reduce 작동 원리 함수
+// return 한 값이 자기자신한테로 넘어 오는것
+const reduce = (predicate, value) => {
+  let result = value;
+  for (let i = 0; i < a.length; i++) {
+    result = predicate(result, a[i]); // result 에 result 자신을 넘기고, 뒤에는 value 값을 넘김
   }
-  return true;
-}
+  return result;
+} 
 
 
-function solution(arr) {
-  let answer = [];
-  for (let x of arr) {
-    let res = 0
-    while (x) {
-      let t = x % 10
-      res = res * 10 + t 
-      x = parseInt(x / 10)
-    }
-    if (isPrime(res)) answer.push(res)
-  }
-  return answer;
-}
+let a = [10, 11, 12, 13, 14, 15]
+
+let answer = a.reduce((acc, v) => { // 첫번째 인수는 누적된것, 두번째가 value 로 들어옴
+  return acc + v; 
+})
 
 
-
-
-let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
-console.log(solution(arr));
+console.log(answer)
