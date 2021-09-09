731f13edad171d47ac6dacbdb86d8846993f4e53
jacobkosmart <jacobkosmart@gmail.com>
2021-09-09 11:15:37 +0900

    edit programer 10

--- _posts/2021-08-30-Programmers_10.md
+++ _posts/2021-08-30-Programmers_10.md
@@ -15,22 +15,86 @@ toc_sticky: true
 
 ## 🔍 문제
 
+한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
+
+각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
+
 ### 🔸 제한사항
 
+- numbers는 길이 1 이상 7 이하인 문자열입니다.
+
+- numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+
+- "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+
 ### 🔹 입출력 예
+
+| numbers | return |
+| ------- | ------ |
+| "17"    | 3      |
+| "011    | 2      |
+
+예제 #1
+[1, 7]으로는 소수 [7, 17, 71]를 만들 수 있습니다.
+
+예제 #2
+[0, 1, 1]으로는 소수 [11, 101]를 만들 수 있습니다.
+
+11과 011은 같은 숫자로 취급합니다.
 
 ---
 
 ## 📌 풀이
 
 ```js
+// 소수 구하는 함수
+function isPrime(num) {
+  // 1이거나 1보다 작을 경우 false
+  if (num <= 1) return false;
+  // 2일경우 true
+  if (num === 2) return true;
+  // 2부터 시작해서 숫자의 제곱근까지 반복 후, i 로 나눈 나머지가 0일 경우 false
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  // 나머지가 있는 경우는 true 임
+  return true;
+}
 
-```
+function solution(numbers) {
+  // 중복이 허용되지 않는 Set 함수
+  let answer = new Set();
+  // "" 을 기준으로 숫자 쪼개기
+  let nums = numbers.split("");
 
-```js
+  function DFS(arr, fixed) {
+    // arr 가 1보다 같거나 클 경우
+    if (arr.length >= 1) {
+      for (let i = 0; i < arr.length; i++) {
+        // 고정값에 배열의 i번째 요소를 합쳐서 새로운 고정값으로 지정
+        const newFixed = fixed + arr[i];
+        // arr 얕은복사
+        const copyArr = [...arr];
+        // newFixed 로 고정한 요소를 배열에서 재거하여, 고정되지 않는 요소들로 배열을 채운다
+        copyArr.splice(i, 1);
+        // answer 안에 이미 값이 없거나 소수일때 newFixed 값을 answer 에 추가 시킨다
+        if (!answer.has(parseInt(newFixed)) && isPrime(parseInt(newFixed))) {
+          answer.add(parseInt(newFixed));
+        }
+        // 고정되지 않는 요소들을 담긴 배열과, 새로운 고정값을 인수로 보내서 제귀 탐색 진행
+        DFS(copyArr, newFixed);
+      }
+    }
+  }
+  DFS(nums, "");
+  return answer.size;
+}
 
+let numbers = "011";
+
+console.log(solution(numbers));
 ```
 
 ## Reference
 
-[]()
+[https://programmers.co.kr/learn/courses/30/lessons/42839](https://programmers.co.kr/learn/courses/30/lessons/42839)
--- _posts/blank.js
+++ _posts/blank.js
@@ -1,63 +1,46 @@
-// function solution(answers) {
-//   let answer = [];
-
-//   let p1 = [1, 2, 3, 4, 5];
-//   let p2 = [2, 1, 2, 3, 2, 4, 2, 5];
-//   let p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-//   let p1C = answers.filter((a, i) => a === p1[i % p1.length]).length;
-//   let p2C = answers.filter((a, i) => a === p2[i % p2.length]).length;
-//   let p3C = answers.filter((a, i) => a === p3[i % p3.length]).length;
-//   let max = Math.max(p1C, p2C, p3C);
-
-//   if (p1C === max) answer.push(1);
-//   if (p2C === max) answer.push(2);
-//   if (p3C === max) answer.push(3);
-
-//   return answer;
-// }
-
-// let answers = [1, 3, 2, 4, 2];
-// console.log(solution(answers));
-
-function solution(answers) {
-  let answer = [];
-  // user array 화 하기
-  let user = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 2, 3, 2, 4, 2, 5],
-    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-  ];
-  // user 수에 맞게 포인트 점수 0으로 초기화
-  let point = Array.from({ length: user.length }, () => 0);
-
-  // console.log(point);
-
-  // 각 user 별 answer 와의 값 비교 i % 각 유저의 길이의 값의 나머지를 맞음 그리고 point 에 1점 추가
-  for (let i = 0; i < answers.length; i++) {
-    if (user[0][i % user[0].length] === answers[i]) {
-      point[0]++;
-    }
-    if (user[1][i % user[1].length] === answers[i]) {
-      point[1]++;
-    }
-    if (user[2][i % user[2].length] === answers[i]) {
-      point[2]++;
-    }
+// 소수 구하는 함수
+function isPrime(num) {
+  // 1이거나 1보다 작을 경우 false
+  if (num <= 1) return false;
+  // 2일경우 true
+  if (num === 2) return true;
+  // 2부터 시작해서 숫자의 제곱근까지 반복 후, i 로 나눈 나머지가 0일 경우 false
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
   }
-
-  // console.log(point);
-
-  // arr 에서 max 값 구하기 spread 문법 사용
-  let max = Math.max(...point);
-  // console.log(max);
-
-  //  max 값과 point 값이 같을 경우 answer 에 번호 입력 (동점 일경우 오름차순 정렬해야되기 때문에 push 로 순차적으로 넣음)
-  for (let i = 0; i < 3; i++) {
-    if (point[i] === max) answer.push(i + 1);
-  }
-  return answer;
+  // 나머지가 있는 경우는 true 임
+  return true;
 }
 
-let answers = [1, 3, 2, 4, 2];
-console.log(solution(answers));
+function solution(numbers) {
+  // 중복이 허용되지 않는 Set 함수
+  let answer = new Set();
+  // "" 을 기준으로 숫자 쪼개기
+  let nums = numbers.split("");
+
+  function DFS(arr, fixed) {
+    // arr 가 1보다 같거나 클 경우
+    if (arr.length >= 1) {
+      for (let i = 0; i < arr.length; i++) {
+        // 고정값에 배열의 i번째 요소를 합쳐서 새로운 고정값으로 지정
+        const newFixed = fixed + arr[i];
+        // arr 얕은복사
+        const copyArr = [...arr];
+        // newFixed 로 고정한 요소를 배열에서 재거하여, 고정되지 않는 요소들로 배열을 채운다
+        copyArr.splice(i, 1);
+        // answer 안에 이미 값이 없거나 소수일때 newFixed 값을 answer 에 추가 시킨다
+        if (!answer.has(parseInt(newFixed)) && isPrime(parseInt(newFixed))) {
+          answer.add(parseInt(newFixed));
+        }
+        // 고정되지 않는 요소들을 담긴 배열과, 새로운 고정값을 인수로 보내서 제귀 탐색 진행
+        DFS(copyArr, newFixed);
+      }
+    }
+  }
+  DFS(nums, "");
+  return answer.size;
+}
+
+let numbers = "011";
+
+console.log(solution(numbers));
