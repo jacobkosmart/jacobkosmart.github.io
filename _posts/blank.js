f2ef1e468c2a8f73cc92dbde908871e2f94937f8
jacobkosmart <jacobkosmart@gmail.com>
2021-09-10 16:06:18 +0900

    edit programer 18

--- _posts/blank.js
+++ _posts/blank.js
@@ -1,23 +1,40 @@
-function solution(numbers, target) {
-  let answer = 0;
+function solution(n, computers) {
+  var answer = 0;
+  // 방문체크를 위한 arr 생성
+  let visited = Array.from({ length: n }, () => false);
 
-  function DFS(index, sum) {
-    if (index === numbers.length) {
-      if (sum === target) {
-        answer++;
+  // 재귀함수
+  function DFS(start) {
+    // 재귀 시작할때 true 로 바꿈
+    visited[start] = true;
+
+    for (let i = 0; i < n; i++) {
+      // 방문하지 않았고, 연결되어 있다면
+      if (!visited[i] && computers[start][i] === 1) {
+        // 재귀 계속 탐색
+        DFS(i);
       }
-      return;
-    } else {
-      DFS(index + 1, sum + numbers[index]);
-      DFS(index + 1, sum - numbers[index]);
     }
   }
 
-  DFS(0, 0);
-
+  // 컴퓨터 갯수만큼 탐색
+  for (let j = 0; j < n; j++) {
+    // 방문을 하지 안았다고 하면
+    if (!visited[j]) {
+      // 제귀함수 시작
+      DFS(j);
+      // 네트워크 개수 추가
+      answer++;
+    }
+  }
   return answer;
 }
 
-let numbers = [1, 1, 1, 1, 1, 1, 1];
-let target = 3;
-console.log(solution(numbers, target));
+let computers = [
+  [1, 1, 0],
+  [1, 1, 0],
+  [0, 0, 1],
+];
+
+let n = 3;
+console.log(solution(n, computers));
