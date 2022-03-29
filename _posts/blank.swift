8f78a1cbeecfd21296b1598bbd45425051fa4436
jacobkosmart <jacobkosmart@gmail.com>
2022-03-29 19:36:32 +0900

    add algorithm-lv1-12

--- _posts/blank.swift
+++ _posts/blank.swift
@@ -1,8 +1,23 @@
 import Foundation
 import SwiftUI
 
-func solution(_ x:Int, _ n:Int) -> [Int64] {
-	return Array(1...n).map { Int64($0 * x) }
+func solution(_ n:Int, _ m:Int) -> [Int] {
+  var num1 = min(n, m)
+	var num2 = max(n, m)
+	var divisor = 2
+	var gcd = 1
+
+	while divisor <= num1 {
+		if num1 % divisor == 0 && num2 % divisor == 0 {
+			num1 = num1 / divisor
+			num2 = num2 / divisor
+			gcd *= divisor
+			divisor = 1
+		}
+		divisor += 1
+	}
+
+	return [gcd, gcd * num1 * num2]
 }
 
-print(solution(2, 5)) // [2, 4, 6, 8, 10]
+print(solution(3, 12)) // [3, 12]
