1db84ec9d54e535098a1223c2fae80450298243e
jacobkosmart <jacobkosmart@gmail.com>
2022-04-15 19:28:35 +0900

    add algorithm-lv1-20

--- _posts/blank.swift
+++ _posts/blank.swift
@@ -1,7 +1,21 @@
 import Foundation
 
-func solution(_ phone_number:String) -> String {
-    return String(repeating: "*", count: phone_number.count - 4) + phone_number.suffix(4)
+func solution(_ s:String, _ n:Int) -> String {
+
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var result = ""
+
+    for letter in s {
+        let isUpperCase = letter.isUppercase
+        
+        if let index = alphabet.firstIndex(of: letter.lowercased()) {
+            let nIndex = index + n > 25 ? index + n - 26 : index + n
+            result += isUpperCase ? alphabet[nIndex].uppercased() : alphabet[nIndex]
+        } else {
+            result += " "
+        }
+    }
+    return result
 }
 
-print(solution("01033334444")) // *******4444
+print(solution("AB", 1)) // "BC"
